e8ef66c9fe7d665c42a027609690aee45d0406cb
Guilherme A. Fôlego <gfolego@gmail.com>
2016-07-15 15:28:39 -0300

    Add license information

--- src/crawler/tidy_dataset.R
+++ src/crawler/tidy_dataset.R
@@ -1,5 +1,25 @@
 #!/usr/bin/Rscript --vanilla
 
+# tidy_dataset.R
+# Copyright (C) 2016
+#   Guilherme Folego (gfolego@gmail.com)
+#   Otavio Gomes (otaviolmiro@gmail.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 # This script makes an attempt to follow Google's R Style Guide
 # For more information, please refer to
 #   https://google-styleguide.googlecode.com/svn/trunk/Rguide.xml
