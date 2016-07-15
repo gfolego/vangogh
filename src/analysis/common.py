ce5f470e715230cf4684327ec0e7b36c73137acc
Guilherme A. Fôlego <gfolego@gmail.com>
2016-07-15 15:33:59 -0300

    Add license information

--- src/analysis/common.py
+++ src/analysis/common.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
-
 
 # common.py
-# Copyright (C) 2015 Guilherme A. Fôlego (gfolego@gmail.com)
+# Copyright (C) 2016
+#   Guilherme Folego (gfolego@gmail.com)
+#   Otavio Gomes (otaviolmiro@gmail.com)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
